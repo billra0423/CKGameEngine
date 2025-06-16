a2f8b24f9c3efb3596f2b323cc624f1846e658e9
billra0423 <billra0423@gmail.com>
2025-06-16 19:58:49 +0900

    commit

---
+++ Assets/Map/PrefebBrush.cs
@@ -0,0 +1,9 @@
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Prefab brush", menuName = "Brushes/Prefab brush")]
+[CustomGridBrush(false, true, false, "Prefab Brush")]
+public class PrefabBrush : GameObjectBrush
+{
+    
+}
